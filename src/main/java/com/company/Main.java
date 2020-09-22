f3d6965b72b5331c0ffb560cf95f3f63545a6b78
hater <n.drozdov1@g.nsu.ru>
2020-09-23 00:12:58 +0700

    fixes

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Client client = new Client("224.0.0.1");
+        Client client = new Client(args[0]);
         while (true) {
             client.work("Hello");
             if(false) break;
